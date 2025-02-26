9a0322c4a476f06e72782fab3ef1b1ebd0811344
Dan Okkels Brendstrup <dob@eaaa.dk>
2025-02-26 14:41:28 +0100

    feat: automatically hash user passwords

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, { type InferSchemaType } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -14,6 +15,21 @@ const userSchema = new mongoose.Schema({
     // Hide the password from the response by default
     select: false,
   },
+});
+
+// Add pre-save middleware to hash password
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) return next();
+
+  try {
+    // Generate a salt and hash the password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
 });
 
 // Infer TypeScript type for the schema
